d0b423edf21b84aac665f2a05f5a2989334f364a
dankosgei <dkosgei6@gmail.com>
2017-07-20 16:06:59 +0300

    created files

--- app/components/display-favorite-book.js
+++ app/components/display-favorite-book.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
+export default Ember.Component.extend ({
+  favoriteBooks: Ember.inject.service(),
+  actions: {
+    removeBookFromFavorites(book) {
+      this.get("favoriteBooks").removeBook(book);
+    }
+  }
 });
